eff7dac690b34980b8ca4561646604be7c2eac71
sk89q <the.sk89q@gmail.com>
2011-02-18 20:54:01 -0800

    Fixed issue with CommandContext breaking arguments with negative numbers.

--- src/com/sk89q/minecraft/util/commands/CommandContext.java
+++ src/com/sk89q/minecraft/util/commands/CommandContext.java
@@ -33,7 +33,7 @@ public class CommandContext {
         int i = 1;
         
         for (; i < args.length; i++) {
-            if (args[i].charAt(0) == '-') {
+            if (args[i].charAt(0) == '-' && args[i].matches("^-[a-zA-Z]+$")) {
                 for (int k = 1; k < args[i].length(); k++) {
                     flags.add(args[i].charAt(k));
                 }
