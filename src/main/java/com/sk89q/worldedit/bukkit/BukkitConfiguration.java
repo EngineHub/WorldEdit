9557d5fbd475a7dcda5331498a1fa0620b01f9cb
sk89q <the.sk89q>
2011-05-01 17:37:28 -0700

    Removed minimum cap on history size.

--- src/main/java/com/sk89q/worldedit/bukkit/BukkitConfiguration.java
+++ src/main/java/com/sk89q/worldedit/bukkit/BukkitConfiguration.java
@@ -81,7 +81,7 @@ public class BukkitConfiguration extends LocalConfiguration {
 
         noOpPermissions = config.getBoolean("no-op-permissions", false);
         
-        LocalSession.MAX_HISTORY_SIZE = Math.max(15, config.getInt("history.size", 15));
+        LocalSession.MAX_HISTORY_SIZE = Math.max(0, config.getInt("history.size", 0));
         LocalSession.EXPIRATION_GRACE = config.getInt("history.expiration", 10) * 60 * 1000;
         
         String snapshotsDir = config.getString("snapshots.directory", "");
