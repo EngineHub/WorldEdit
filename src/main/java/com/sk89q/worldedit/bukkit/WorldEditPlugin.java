b8a987b20488e79e98ca3a525662f60916cbc959
Wizjany <wizjany@gmail.com>
2011-08-07 11:57:32 -0400

    Cancel scheduler tasks onDisable to prevent nag warnings

--- src/main/java/com/sk89q/worldedit/bukkit/WorldEditPlugin.java
+++ src/main/java/com/sk89q/worldedit/bukkit/WorldEditPlugin.java
@@ -118,6 +118,7 @@ public class WorldEditPlugin extends JavaPlugin {
         }
         controller.clearSessions();
         config.unload();
+        this.getServer().getScheduler().cancelTasks(this);
     }
     
     /**
