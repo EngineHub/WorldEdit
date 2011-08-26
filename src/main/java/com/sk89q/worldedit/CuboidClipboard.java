c8bbcbd1338f99c2fce22aa49ec2447189604115
TomyLobo <anon@anon.com>
2011-08-26 14:04:38 +0200

    Fixed flipping of the central blocks in a clipboard with odd length.

--- src/main/java/com/sk89q/worldedit/CuboidClipboard.java
+++ src/main/java/com/sk89q/worldedit/CuboidClipboard.java
@@ -182,7 +182,7 @@ public class CuboidClipboard {
 
         switch (dir) {
         case NORTH_SOUTH:
-            int wid = (int)Math.ceil(width / 2);
+            int wid = (int)Math.ceil(width / 2.0f);
             for (int xs = 0; xs < wid; ++xs) {
                 for (int z = 0; z < length; ++z) {
                     for (int y = 0; y < height; ++y) {
@@ -208,7 +208,7 @@ public class CuboidClipboard {
             break;
 
         case WEST_EAST:
-            int len = (int)Math.ceil(length / 2);
+            int len = (int)Math.ceil(length / 2.0f);
             for (int zs = 0; zs < len; ++zs) {
                 for (int x = 0; x < width; ++x) {
                     for (int y = 0; y < height; ++y) {
@@ -233,7 +233,7 @@ public class CuboidClipboard {
             break;
 
         case UP_DOWN:
-            int hei = (int)Math.ceil(height / 2);
+            int hei = (int)Math.ceil(height / 2.0f);
             for (int ys = 0; ys < hei; ++ys) {
                 for (int x = 0; x < width; ++x) {
                     for (int z = 0; z < length; ++z) {
