780d07a9ba6d970b1f0dd34ed080978a1a7a53be
TomyLobo <anon@anon.com>
2011-09-02 21:32:23 +0200

    Fixed a warning. Seriously, guys...

--- src/main/java/com/sk89q/worldedit/WorldEditPermissionException.java
+++ src/main/java/com/sk89q/worldedit/WorldEditPermissionException.java
@@ -4,6 +4,8 @@ package com.sk89q.worldedit;
  * @author zml2008
  */
 public class WorldEditPermissionException extends WorldEditException {
+    private static final long serialVersionUID = 1L;
+
     public WorldEditPermissionException() {
         super("You don't have permission to do this.");
     }
