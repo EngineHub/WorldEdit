b2ff0fb2a2113ba3fb6ff0da737ea3827d7b5bf4
ragd <mobile@nhdn.de>
2012-08-24 19:46:39 +0200

    Added support for rotating wooden logs

--- src/main/java/com/sk89q/worldedit/blocks/BlockData.java
+++ src/main/java/com/sk89q/worldedit/blocks/BlockData.java
@@ -143,6 +143,10 @@ public final class BlockData {
             }
             break;
 
+        case BlockID.LOG:
+            if (data >= 4 && data <= 11) data ^= 0xc;
+            break;
+
         case BlockID.REDSTONE_REPEATER_OFF:
         case BlockID.REDSTONE_REPEATER_ON:
             int dir = data & 0x03;
@@ -311,6 +315,10 @@ public final class BlockData {
             case 3: return 2;
             case 0: return 3;
             }
+            break;
+
+        case BlockID.LOG:
+            if (data >= 4 && data <= 11) data ^= 0xc;
             break;
 
         case BlockID.REDSTONE_REPEATER_OFF:
