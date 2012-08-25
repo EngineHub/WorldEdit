9d5e75b9ecda11ae51439e6a393f0ddfe22160f9
sk89q <the.sk89q>
2012-08-24 19:16:43 -0700

    Fixed //gen not having min. arg count of 2.

--- src/main/java/com/sk89q/worldedit/commands/GenerationCommands.java
+++ src/main/java/com/sk89q/worldedit/commands/GenerationCommands.java
@@ -19,12 +19,20 @@
 
 package com.sk89q.worldedit.commands;
 
+import static com.sk89q.minecraft.util.commands.Logging.LogMode.ALL;
+import static com.sk89q.minecraft.util.commands.Logging.LogMode.PLACEMENT;
+import static com.sk89q.minecraft.util.commands.Logging.LogMode.POSITION;
+
 import com.sk89q.minecraft.util.commands.Command;
 import com.sk89q.minecraft.util.commands.CommandContext;
 import com.sk89q.minecraft.util.commands.CommandPermissions;
 import com.sk89q.minecraft.util.commands.Logging;
-import static com.sk89q.minecraft.util.commands.Logging.LogMode.*;
-import com.sk89q.worldedit.*;
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.LocalPlayer;
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.expression.ExpressionException;
 import com.sk89q.worldedit.patterns.Pattern;
 import com.sk89q.worldedit.regions.Region;
@@ -335,7 +343,7 @@ public class GenerationCommands {
             "If neither -r nor -o is given, the selection is mapped to -1..1\n" +
             "See also tinyurl.com/wesyntax.",
         flags = "hroc",
-        min = 1,
+        min = 2,
         max = -1
     )
     @CommandPermissions("worldedit.generation.shape")
