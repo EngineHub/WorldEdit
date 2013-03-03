7eb100a0e24d636063329c55df86fc18fef5e5ee
sk89q <the.sk89q@gmail.com>
2013-03-02 16:41:39 -0800

    Start new WorldEdit instance class.

---
+++ src/main/java/org/enginehub/worldedit/WorldEdit.java
@@ -0,0 +1,47 @@
+// $Id$
+/*
+ * This file is a part of WorldEdit.
+ * Copyright (c) sk89q <http://www.sk89q.com>
+ * Copyright (c) the WorldEdit team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.enginehub.worldedit;
+
+/**
+ * An instance of WorldEdit.
+ */
+public final class WorldEdit {
+
+    private static WorldEdit instance;
+
+    private WorldEdit() {
+    }
+
+    /**
+     * Get the singleton for WorldEdit.
+     *
+     * @return singleton
+     */
+    public synchronized static WorldEdit getInstance() {
+        if (instance == null) {
+            instance = new WorldEdit();
+        }
+
+        return instance;
+    }
+
+
+
+
+}
