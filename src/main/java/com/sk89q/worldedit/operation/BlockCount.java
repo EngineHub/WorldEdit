79643790ccfa46e4cd48ea0cc8527be27e01601c
sk89q <the.sk89q@gmail.com>
2014-03-26 23:09:24 -0700

    Added BlockCount, which counts the number of blocks matching a mask.

---
+++ src/main/java/com/sk89q/worldedit/operation/BlockCount.java
@@ -0,0 +1,64 @@
+/*
+ * WorldEdit, a Minecraft world manipulation toolkit
+ * Copyright (C) sk89q <http://www.sk89q.com>
+ * Copyright (C) WorldEdit team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sk89q.worldedit.operation;
+
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.masks.Mask;
+
+/**
+ * Counts the number of blocks that match a given mask.
+ */
+ public class BlockCount implements RegionFunction {
+
+    private final EditSession editSession;
+    private Mask mask;
+    private int count;
+
+    /**
+     * Create a new block counter.
+     *
+     * @param editSession the edit session
+     * @param mask the mask
+     */
+    public BlockCount(EditSession editSession, Mask mask) {
+        this.editSession = editSession;
+        this.mask = mask;
+    }
+    
+    /**
+     * Returns the number of blocks that have been counted.
+     *
+     * @return the number of blocks
+     */
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public boolean apply(Vector position) throws WorldEditException {
+        if (mask.matches(editSession, position)) {
+            count++;
+        }
+        return false;
+    }
+
+}
