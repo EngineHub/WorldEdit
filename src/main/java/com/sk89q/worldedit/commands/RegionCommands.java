f48040572e24177807e7e4586c57df8c93de0c16
sk89q <the.sk89q@gmail.com>
2014-03-30 13:50:07 -0700

    Fixed LayerVisitor constructors in RegionCommands.

--- src/main/java/com/sk89q/worldedit/commands/RegionCommands.java
+++ src/main/java/com/sk89q/worldedit/commands/RegionCommands.java
@@ -552,8 +552,7 @@ public class RegionCommands {
 
         ForestGenerator generator = new ForestGenerator(editSession, new TreeGenerator(type));
         GroundFunction ground = new GroundFunction(editSession, generator);
-        LayerVisitor visitor = new LayerVisitor(
-                editSession, asFlatRegion(region), minimumBlockY(region), maximumBlockY(region), ground);
+        LayerVisitor visitor = new LayerVisitor(asFlatRegion(region), minimumBlockY(region), maximumBlockY(region), ground);
         visitor.setMask(new NoiseFilter2D(new RandomNoise(), density));
         OperationHelper.completeLegacy(visitor);
 
@@ -575,8 +574,7 @@ public class RegionCommands {
         Region region = session.getSelection(player.getWorld());
         FloraGenerator generator = new FloraGenerator(editSession);
         GroundFunction ground = new GroundFunction(editSession, generator);
-        LayerVisitor visitor = new LayerVisitor(
-                editSession, asFlatRegion(region), minimumBlockY(region), maximumBlockY(region), ground);
+        LayerVisitor visitor = new LayerVisitor(asFlatRegion(region), minimumBlockY(region), maximumBlockY(region), ground);
         visitor.setMask(new NoiseFilter2D(new RandomNoise(), density));
         OperationHelper.completeLegacy(visitor);
 
