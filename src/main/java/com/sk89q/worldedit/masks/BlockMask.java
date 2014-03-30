e13d5198be70b1e97625084fed6e386630e511d0
sk89q <the.sk89q@gmail.com>
2014-03-29 18:31:34 -0700

    Added a BlockMask(BaseBlock... block) constructor.

--- src/main/java/com/sk89q/worldedit/masks/BlockMask.java
+++ src/main/java/com/sk89q/worldedit/masks/BlockMask.java
@@ -1,14 +1,15 @@
 package com.sk89q.worldedit.masks;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.blocks.BaseBlock;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 public class BlockMask extends AbstractMask {
+
     private final Set<BaseBlock> blocks;
 
     public BlockMask() {
@@ -17,6 +18,13 @@ public class BlockMask extends AbstractMask {
 
     public BlockMask(Set<BaseBlock> types) {
         this.blocks = types;
+    }
+
+    public BlockMask(BaseBlock... block) {
+        blocks = new HashSet<BaseBlock>();
+        for (BaseBlock b : block) {
+            add(b);
+        }
     }
 
     public BlockMask(BaseBlock block) {
@@ -35,7 +43,8 @@ public class BlockMask extends AbstractMask {
     @Override
     public boolean matches(EditSession editSession, Vector pos) {
         BaseBlock block = editSession.getBlock(pos);
-        return  blocks.contains(block)
+        return blocks.contains(block)
                 || blocks.contains(new BaseBlock(block.getType(), -1));
-   }
+    }
+
 }
