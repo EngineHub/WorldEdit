05424c3745554d3133bb5ac2a7e2556dd536c32b
griffrez <grifferz2@gmail.com>
2016-12-03 01:01:24 +0000

    Changed to a simpler expression
    Also merged the lines

--- worldedit-core/src/main/java/com/sk89q/worldedit/EditSession.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/EditSession.java
@@ -961,15 +961,11 @@ public class EditSession implements Extent {
         checkNotNull(pattern);
 
         Vector center = region.getCenter();
-        Vector vec1 = new Vector((int) center.getX(), (int) center.getY(), (int) center.getZ());
         Vector vec2 = center.toBlockVector();
-        boolean condX = (region.getWidth() % 2) == 0;
-        boolean condZ = (region.getLength() % 2) == 0;
-        Vector vec3 = new Vector(vec2.getX() + (condX ? 0.5*Math.signum(vec2.getX()) : 0), vec2.getY(), vec2.getZ() + (condZ ? 0.5*Math.signum(vec2.getZ()) : 0));
         Region centerRegion = new CuboidRegion(
                 getWorld(), // Causes clamping of Y range
-                vec1,
-                vec3);
+                new Vector((int) center.getX(), (int) center.getY(), (int) center.getZ()),
+                new Vector(Math.signum(vec2.getX())*Math.round(Math.abs(vec2.getX())), vec2.getY(), Math.signum(vec2.getZ())*Math.round(Math.abs(vec2.getZ()))));
         return setBlocks(centerRegion, pattern);
     }
 
