3b5972b7f2e60a8e84016f2eeb4b575401674a4c
Kenzie Togami <ket1999@gmail.com>
2018-10-30 18:06:00 -0700

    Flush / unbuffer more tools

--- worldedit-core/src/main/java/com/sk89q/worldedit/command/tool/TreePlanter.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/command/tool/TreePlanter.java
@@ -48,25 +48,25 @@ public class TreePlanter implements BlockTool {
     @Override
     public boolean actPrimary(Platform server, LocalConfiguration config, Player player, LocalSession session, Location clicked) {
 
-        EditSession editSession = session.createEditSession(player);
+        try (EditSession editSession = session.createEditSession(player)) {
+            try {
+                boolean successful = false;
 
-        try {
-            boolean successful = false;
-            
-            for (int i = 0; i < 10; i++) {
-                if (treeType.generate(editSession, clicked.toVector().add(0, 1, 0))) {
-                    successful = true;
-                    break;
+                for (int i = 0; i < 10; i++) {
+                    if (treeType.generate(editSession, clicked.toVector().add(0, 1, 0))) {
+                        successful = true;
+                        break;
+                    }
                 }
+
+                if (!successful) {
+                    player.printError("A tree can't go there.");
+                }
+            } catch (MaxChangedBlocksException e) {
+                player.printError("Max. blocks changed reached.");
+            } finally {
+                session.remember(editSession);
             }
-            
-            if (!successful) {
-                player.printError("A tree can't go there.");
-            }
-        } catch (MaxChangedBlocksException e) {
-            player.printError("Max. blocks changed reached.");
-        } finally {
-            session.remember(editSession);
         }
 
         return true;
