449b0991f33e794a90f13cb7b0bfe57cd193ac82
Matthew Miller <mnmiller1@me.com>
2019-07-22 21:20:40 +1000

    Bump to Piston 0.4.3

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val TEXT = "3.0.1"
     const val TEXT_EXTRAS = "3.0.2"
-    const val PISTON = "0.4.2"
+    const val PISTON = "0.4.3"
     const val AUTO_VALUE = "1.6.5"
     const val JUNIT = "5.5.0"
 }
