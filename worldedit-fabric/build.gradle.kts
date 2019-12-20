e9dc8befc7eaa5153d658635ea54fe27fb406d99
Octavia Togami <octavia.togami@gmail.com>
2019-12-20 15:23:18 -0800

    [Fabric] Update to 1.15.1

--- worldedit-fabric/build.gradle.kts
+++ worldedit-fabric/build.gradle.kts
@@ -6,9 +6,9 @@ applyShadowConfiguration()
 
 apply(plugin = "fabric-loom")
 
-val minecraftVersion = "1.15"
-val yarnMappings = "1.15+build.1"
-val loaderVersion = "0.7.2+build.174"
+val minecraftVersion = "1.15.1"
+val yarnMappings = "1.15.1+build.6"
+val loaderVersion = "0.7.2+build.175"
 
 configurations.all {
     resolutionStrategy {
