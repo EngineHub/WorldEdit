20e3afbc47bffeb094ddd66a7e67841e97daec1d
wizjany <wizjany@gmail.com>
2020-01-03 23:55:40 -0500

    Expose legacy data value.
    Fixes WORLDEDIT-4010.

--- worldedit-core/src/main/java/com/sk89q/worldedit/command/tool/QueryTool.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/command/tool/QueryTool.java
@@ -34,6 +34,7 @@ import com.sk89q.worldedit.util.formatting.text.event.HoverEvent;
 import com.sk89q.worldedit.util.formatting.text.format.TextColor;
 import com.sk89q.worldedit.world.World;
 import com.sk89q.worldedit.world.block.BaseBlock;
+import com.sk89q.worldedit.world.registry.LegacyMapper;
 
 import java.util.OptionalInt;
 
@@ -63,10 +64,16 @@ public class QueryTool implements BlockTool {
         final OptionalInt internalId = BlockStateIdAccess.getBlockStateId(block.toImmutableState());
         if (internalId.isPresent()) {
             builder.append(TextComponent.of(" (" + internalId.getAsInt() + ") ", TextColor.DARK_GRAY)
-                .hoverEvent(HoverEvent.of(HoverEvent.Action.SHOW_TEXT, TranslatableComponent.of("worldedit.tool.info.internalid.hover"))));
+                    .hoverEvent(HoverEvent.of(HoverEvent.Action.SHOW_TEXT, TranslatableComponent.of("worldedit.tool.info.internalid.hover"))));
         }
+        final int[] legacy = LegacyMapper.getInstance().getLegacyFromBlock(block.toImmutableState());
+        if (legacy != null) {
+            builder.append(TextComponent.of(" (" + legacy[0] + ":" + legacy[1] + ") ", TextColor.DARK_GRAY)
+                    .hoverEvent(HoverEvent.of(HoverEvent.Action.SHOW_TEXT, TranslatableComponent.of("worldedit.tool.info.legacy.hover"))));
+        }
+
         builder.append(TextComponent.of(" (" + world.getBlockLightLevel(blockPoint) + "/"
-                    + world.getBlockLightLevel(blockPoint.add(0, 1, 0)) + ")", TextColor.WHITE)
+                + world.getBlockLightLevel(blockPoint.add(0, 1, 0)) + ")", TextColor.WHITE)
                 .hoverEvent(HoverEvent.of(HoverEvent.Action.SHOW_TEXT, TranslatableComponent.of("worldedit.tool.info.light.hover"))));
 
         player.print(builder.build());
