EngineHub/WorldEdit

commit 4123eddff10145989dd67271d153d9e1e9fd4230

Few minor tweaks to WNA that don't modify behaviour but bring it more inline with how MC works

worldedit-core/src/main/java/com/sk89q/worldedit/internal/wna/WorldNativeAccess.java
@@ -59,8 +59,8 @@ public interface WorldNativeAccess<NC, NBS, NP> {
         if (sideEffects.shouldApply(SideEffect.VALIDATION)) {
             newState = getValidBlockForPosition(newState, pos);
         }
-        NBS successState = setBlockState(chunk, pos, newState);
-        boolean successful = successState != null;
+        NBS lastValue = setBlockState(chunk, pos, newState);
+        boolean successful = lastValue != null;
 
         // Create the TileEntity
         if (successful || old == newState) {
@@ -84,7 +84,7 @@ public interface WorldNativeAccess<NC, NBS, NP> {
             if (sideEffects.getState(SideEffect.LIGHTING) == SideEffect.State.ON) {
                 updateLightingForBlock(pos);
             }
-            markAndNotifyBlock(pos, chunk, old, newState, sideEffects);
+            markAndNotifyBlock(pos, chunk, lastValue, newState, sideEffects);
         }
 
         return successful;
@@ -183,7 +183,7 @@ public interface WorldNativeAccess<NC, NBS, NP> {
         }
 
         // Seems used only for PoI updates
-        onBlockStateChange(pos, oldState, newState);
+        onBlockStateChange(pos, oldState, blockState1);
     }
 
 }

commit 7f82a7d666658e75e9e7faa7770be19c76c7e435

Fix issue introduced by 4123eddff
lastValue may be null at this point, old is not

worldedit-core/src/main/java/com/sk89q/worldedit/internal/wna/WorldNativeAccess.java
@@ -84,7 +84,7 @@ public interface WorldNativeAccess<NC, NBS, NP> {
             if (sideEffects.getState(SideEffect.LIGHTING) == SideEffect.State.ON) {
                 updateLightingForBlock(pos);
             }
-            markAndNotifyBlock(pos, chunk, lastValue, newState, sideEffects);
+            markAndNotifyBlock(pos, chunk, old, newState, sideEffects);
         }
 
         return successful;