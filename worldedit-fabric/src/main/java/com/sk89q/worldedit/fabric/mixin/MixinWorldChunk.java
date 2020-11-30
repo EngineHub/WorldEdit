f578a7fed83477e35591b7e1845dd949a7412c39
Octavia Togami <octavia.togami@gmail.com>
2020-11-29 17:45:44 -0800

    Fix a minor threading problem

--- worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/mixin/MixinWorldChunk.java
+++ worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/mixin/MixinWorldChunk.java
@@ -1,7 +1,27 @@
+/*
+ * WorldEdit, a Minecraft world manipulation toolkit
+ * Copyright (C) sk89q <http://www.sk89q.com>
+ * Copyright (C) WorldEdit team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.sk89q.worldedit.fabric.mixin;
 
 import com.sk89q.worldedit.fabric.internal.ExtendedChunk;
 import net.minecraft.block.BlockState;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
@@ -38,7 +58,16 @@ public abstract class MixinWorldChunk implements Chunk, ExtendedChunk {
         at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;onBlockAdded(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Z)V", ordinal = 0)
     )
     public void setBlockStateHook(BlockState target, World world, BlockPos pos, BlockState old, boolean move) {
-        if (shouldUpdate) {
+        boolean localShouldUpdate;
+        MinecraftServer server = world.getServer();
+        if (server == null || Thread.currentThread() != server.getThread()) {
+            // We're not on the server thread for some reason, WorldEdit will never be here
+            // so we'll just ignore our flag
+            localShouldUpdate = true;
+        } else {
+            localShouldUpdate = shouldUpdate;
+        }
+        if (localShouldUpdate) {
             target.onBlockAdded(world, pos, old, move);
         }
     }
