dd32e43bfbf70c359bfaa99edadbbda775f686de
Matthew Miller <mnmiller1@me.com>
2020-12-21 21:15:18 +1000

    Fix SideEffectSet.none() being the default set

--- worldedit-core/src/main/java/com/sk89q/worldedit/util/SideEffectSet.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/util/SideEffectSet.java
@@ -30,10 +30,10 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class SideEffectSet {
-    private static final SideEffectSet DEFAULT = new SideEffectSet(
-            Arrays.stream(SideEffect.values()).collect(Collectors.toMap(Function.identity(), SideEffect::getDefaultValue))
+    private static final SideEffectSet DEFAULT = new SideEffectSet();
+    private static final SideEffectSet NONE = new SideEffectSet(
+        Arrays.stream(SideEffect.values()).collect(Collectors.toMap(Function.identity(), state -> SideEffect.State.OFF))
     );
-    private static final SideEffectSet NONE = new SideEffectSet();
 
     private final Map<SideEffect, SideEffect.State> sideEffects;
     private final Set<SideEffect> appliedSideEffects;
