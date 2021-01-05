7be7b7cd67dc6613cc5006feca9b96fad3efb3c4
Matthew Miller <mnmiller1@me.com>
2021-01-05 16:19:45 +1000

    Add a command to apply side effects (#1623)
    * [WIP] Add a command to apply side effects
    
    * Fix checkstyle
    
    * Switch to the //update command, and add messages
    
    * The update command won't change in the future, remove that warning
    
    * Create a Function that applies side effects for easier API usage, and make gmasks apply to `//update`
    
    * Fixed `//update` not using the current side effect set
    
    * Fixed PR review notes
    
    * Fixed regression in last commit
    
    * smh why is it null
    
    * Update GeneralCommands.java
    
    * PR review
    
    * PR review
    
    * :(
    
    * :(

---
+++ worldedit-core/src/main/java/com/sk89q/worldedit/function/block/ApplySideEffect.java
@@ -0,0 +1,50 @@
+/*
+ * WorldEdit, a Minecraft world manipulation toolkit
+ * Copyright (C) sk89q <http://www.sk89q.com>
+ * Copyright (C) WorldEdit team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.sk89q.worldedit.function.block;
+
+import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.function.RegionFunction;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.util.SideEffectSet;
+import com.sk89q.worldedit.world.World;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Applies the given side effect within a region.
+ */
+public class ApplySideEffect implements RegionFunction {
+
+    private final World world;
+    private final SideEffectSet sideEffectSet;
+
+    public ApplySideEffect(World world, SideEffectSet sideEffectSet) {
+        checkNotNull(world);
+        checkNotNull(sideEffectSet);
+        this.world = world;
+        this.sideEffectSet = sideEffectSet;
+    }
+
+    @Override
+    public boolean apply(BlockVector3 position) throws WorldEditException {
+        world.applySideEffects(position, world.getBlock(position), sideEffectSet);
+        return true;
+    }
+}
