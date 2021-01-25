8f2f37b7bc2d5cd78b086e3d590ce5ece995f650
Octavia Togami <octavia.togami@gmail.com>
2021-01-25 00:33:26 -0800

    Drop the fabric force stuff

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -26,17 +26,6 @@ repositories {
     }
 }
 
-configurations.all {
-    resolutionStrategy {
-        // Fabric needs this.
-        force(
-            "commons-io:commons-io:2.6",
-            "org.ow2.asm:asm:8.0.1",
-            "org.ow2.asm:asm-commons:8.0.1"
-        )
-    }
-}
-
 val properties = Properties().also { props ->
     project.projectDir.resolveSibling("gradle.properties").bufferedReader().use {
         props.load(it)
