043c3315aa984e6d845ead4c9e372fabaf0bb8aa
Octavia Togami <octavia.togami@gmail.com>
2021-06-13 11:32:38 -0700

    Fix Scuffed POMs (#1779)
    * Repair fabric POM
    
    * Fix-up a ton of publishing errors
    
    * Remove shadow jar from publishing
    
    * Remove shadow application from platform/core

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,7 @@ rootProject.name = "worldedit"
 
 include("worldedit-libs")
 
-// Forge has been removed until FG 5 is available.
-listOf("bukkit", "core", "sponge", "fabric", "cli").forEach {
+listOf("bukkit", "core", "sponge", "fabric", "forge", "cli").forEach {
     include("worldedit-libs:$it")
     include("worldedit-$it")
 }
