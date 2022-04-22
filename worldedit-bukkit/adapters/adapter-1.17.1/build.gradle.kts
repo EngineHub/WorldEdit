7aa69db969f3131e398b548337e3ed2eb6e70786
Octavia Togami <octavia.togami@gmail.com>
2022-04-21 18:40:49 -0700

    Bump paperweight + bundle to fix build

--- worldedit-bukkit/adapters/adapter-1.17.1/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1.17.1/build.gradle.kts
@@ -1,5 +1,5 @@
 applyPaperweightAdapterConfiguration(javaRelease = 17)
 
 dependencies {
-    paperDevBundle("1.17.1-R0.1-20211219.175449-201")
+    paperDevBundle("1.17.1-R0.1-20220414.034903-210")
 }
