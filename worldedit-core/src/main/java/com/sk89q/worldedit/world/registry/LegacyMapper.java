d3a671e2d745cf6653fdc5f8747dd867cddcac96
Madeline Miller <mnmiller1@me.com>
2022-08-13 17:56:13 +1000

    Micro-optimisation to reduce identical duplicate map lookups

--- worldedit-core/src/main/java/com/sk89q/worldedit/world/registry/LegacyMapper.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/world/registry/LegacyMapper.java
@@ -154,7 +154,7 @@ public final class LegacyMapper {
     @Nullable
     public ItemType getItemFromLegacy(int legacyId, int data) {
         ItemType foundItem = stringToItemMap.get(legacyId + ":" + data);
-        if (foundItem == null) {
+        if (foundItem == null && data != 0) {
             // Some schematics contain invalid data values, so try without the data value
             return stringToItemMap.get(legacyId + ":" + 0);
         }
@@ -179,7 +179,7 @@ public final class LegacyMapper {
     @Nullable
     public BlockState getBlockFromLegacy(int legacyId, int data) {
         BlockState foundBlock = stringToBlockMap.get(legacyId + ":" + data);
-        if (foundBlock == null) {
+        if (foundBlock == null && data != 0) {
             // Some schematics contain invalid data values, so try without the data value
             return stringToBlockMap.get(legacyId + ":" + 0);
         }
