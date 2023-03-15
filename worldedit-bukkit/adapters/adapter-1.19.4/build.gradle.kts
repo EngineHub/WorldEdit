632470b03b6d1ac24bb3977de04d5360dcd3f647
JOO200 <github@joo200.de>
2023-03-15 23:32:18 +0100

    bukkit: add adapter for bukkit 1.19.4 (#2281)
    * bukkit: add adapter for bukkit 1.19.4
    
    * added missing reobf mapping to 1.19.4 adapter
    
    * Drop unused comments

---
+++ worldedit-bukkit/adapters/adapter-1.19.4/build.gradle.kts
@@ -0,0 +1,6 @@
+applyPaperweightAdapterConfiguration()
+
+dependencies {
+    // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
+    paperDevBundle("1.19.4-R0.1-20230315.180941-2")
+}
