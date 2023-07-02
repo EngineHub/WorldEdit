93e413c6138fafa979982ee5ae99b8959a25ff0a
Madeline Miller <mnmiller1@me.com>
2023-07-02 20:55:16 +1000

    Fix test compile error due to Spigot mid-version API break

--- worldedit-bukkit/src/test/java/com/sk89q/wepif/TestOfflinePermissible.java
+++ worldedit-bukkit/src/test/java/com/sk89q/wepif/TestOfflinePermissible.java
@@ -19,6 +19,7 @@
 
 package com.sk89q.wepif;
 
+import org.bukkit.BanEntry;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
@@ -32,6 +33,7 @@ import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.profile.PlayerProfile;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Locale;
@@ -152,6 +154,11 @@ public class TestOfflinePermissible implements OfflinePlayer, Permissible {
 
     @Override
     public boolean isBanned() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public BanEntry<PlayerProfile> ban(String reason, Date expires, String source) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
