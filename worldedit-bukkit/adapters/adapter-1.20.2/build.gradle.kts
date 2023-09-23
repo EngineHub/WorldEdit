964dcb9b44e91cc99bf901c44fb0e2d6765052ae
Madeline Miller <mnmiller1@me.com>
2023-09-23 16:13:35 +1000

    [Bukkit] 1.20.2

---
+++ worldedit-bukkit/adapters/adapter-1.20.2/build.gradle.kts
@@ -0,0 +1,8 @@
+import io.papermc.paperweight.userdev.PaperweightUserDependenciesExtension
+
+applyPaperweightAdapterConfiguration()
+
+dependencies {
+    // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
+    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.2-R0.1-20230922.233713-3")
+}
