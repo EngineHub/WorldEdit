3acbcd1c89c37ed17a9e7761efd3fca7f6985ed0
Zeranny <danielhorn1@hotmail.co.uk>
2023-10-15 00:16:40 +0100

    Fix ellipsoid and cylinder regions not fully clearing

--- worldedit-core/src/main/java/com/sk89q/worldedit/regions/selector/CylinderRegionSelector.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/regions/selector/CylinderRegionSelector.java
@@ -247,6 +247,8 @@ public class CylinderRegionSelector implements RegionSelector, CUIRegion {
     @Override
     public void clear() {
         region = new CylinderRegion(region.getWorld());
+        selectedCenter = false;
+        selectedRadius = false;
     }
 
     @Override
