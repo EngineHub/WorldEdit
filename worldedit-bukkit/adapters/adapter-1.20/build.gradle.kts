607b9fce78eed2cab56c46ef74426842104dcbd9
Madeline Miller <mnmiller1@me.com>
2023-11-21 22:39:42 +1000

    Update Gradle and Gradle plugins

--- worldedit-bukkit/adapters/adapter-1.20/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1.20/build.gradle.kts
@@ -4,5 +4,5 @@ applyPaperweightAdapterConfiguration()
 
 dependencies {
     // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.1-R0.1-20230816.041652-110")
+    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.1-R0.1-20230921.165944-178")
 }
