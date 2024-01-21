24050aacb07e75d27266148c18ca40502184786c
Octavia Togami <octavia.togami@gmail.com>
2024-01-21 15:51:21 -0800

    Update Gradle plugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,7 +2,6 @@ import java.util.Properties
 
 plugins {
     `kotlin-dsl`
-    kotlin("jvm") version embeddedKotlinVersion
 }
 
 repositories {
@@ -47,15 +46,15 @@ val mixinVersion: String = properties.getProperty("mixin.version")
 dependencies {
     implementation(gradleApi())
     implementation("gradle.plugin.org.cadixdev.gradle:licenser:0.6.1")
-    implementation("org.ajoberstar.grgit:grgit-gradle:4.1.1")
+    implementation("org.ajoberstar.grgit:grgit-gradle:5.2.1")
     implementation("com.github.johnrengelman:shadow:8.1.1")
-    implementation("org.jfrog.buildinfo:build-info-extractor-gradle:4.32.0")
+    implementation("org.jfrog.buildinfo:build-info-extractor-gradle:5.1.14")
     implementation("org.spongepowered:spongegradle-plugin-development:2.2.0")
     implementation("org.spongepowered:vanillagradle:0.2.1-20231105.223944-69")
-    implementation("net.minecraftforge.gradle:ForgeGradle:6.0.16")
+    implementation("net.minecraftforge.gradle:ForgeGradle:6.0.20")
     implementation("net.fabricmc:fabric-loom:$loomVersion")
     implementation("net.fabricmc:sponge-mixin:$mixinVersion")
     implementation("org.enginehub.gradle:gradle-codecov-plugin:0.2.0")
-    implementation("io.papermc.paperweight.userdev:io.papermc.paperweight.userdev.gradle.plugin:1.5.9")
+    implementation("io.papermc.paperweight.userdev:io.papermc.paperweight.userdev.gradle.plugin:1.5.11")
     implementation("org.spongepowered:mixingradle:0.7.38")
 }
