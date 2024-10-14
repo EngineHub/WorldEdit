dd3e1fb79e32f227b392d0a72843a6d1e4e43af1
Octavia Togami <octavia.togami@gmail.com>
2024-10-13 18:05:52 -0700

    [Fabric] 1.21.2-pre3

--- worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/FabricItemCategoryRegistry.java
+++ worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/FabricItemCategoryRegistry.java
@@ -34,7 +34,7 @@ public class FabricItemCategoryRegistry implements ItemCategoryRegistry {
     @Override
     public Set<ItemType> getCategorisedByName(String category) {
         return FabricWorldEdit.getRegistry(Registries.ITEM)
-            .getTag(TagKey.create(Registries.ITEM, ResourceLocation.parse(category)))
+            .get(TagKey.create(Registries.ITEM, ResourceLocation.parse(category)))
             .stream()
             .flatMap(HolderSet.Named::stream)
             .map(Holder::value)
