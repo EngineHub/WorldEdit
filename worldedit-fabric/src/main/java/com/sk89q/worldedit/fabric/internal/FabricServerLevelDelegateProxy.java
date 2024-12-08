5a91d26b0116217ae075e3c17783efe7e7bc2aca
Octavia Togami <octavia.togami@gmail.com>
2024-12-08 13:09:01 -0800

    [wip] Fix block entities in features/structures
    Fixes #2669

--- worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/internal/FabricServerLevelDelegateProxy.java
+++ worldedit-fabric/src/main/java/com/sk89q/worldedit/fabric/internal/FabricServerLevelDelegateProxy.java
@@ -23,13 +23,15 @@ import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.MaxChangedBlocksException;
 import com.sk89q.worldedit.entity.BaseEntity;
 import com.sk89q.worldedit.fabric.FabricAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.math.Vector3;
 import com.sk89q.worldedit.util.Location;
-import com.sk89q.worldedit.world.block.BlockTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
@@ -37,40 +39,45 @@ import org.jetbrains.annotations.Nullable;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
 
-public class FabricServerLevelDelegateProxy implements InvocationHandler {
+public class FabricServerLevelDelegateProxy implements InvocationHandler, AutoCloseable {
 
     private final EditSession editSession;
     private final ServerLevel serverLevel;
+    private final Map<BlockVector3, BlockEntity> createdBlockEntities = new HashMap<>();
 
     private FabricServerLevelDelegateProxy(EditSession editSession, ServerLevel serverLevel) {
         this.editSession = editSession;
         this.serverLevel = serverLevel;
     }
 
-    public static WorldGenLevel newInstance(EditSession editSession, ServerLevel serverLevel) {
-        return (WorldGenLevel) Proxy.newProxyInstance(
-            serverLevel.getClass().getClassLoader(),
-            serverLevel.getClass().getInterfaces(),
-            new FabricServerLevelDelegateProxy(editSession, serverLevel)
+    public record LevelAndProxy(WorldGenLevel level, FabricServerLevelDelegateProxy proxy) implements AutoCloseable {
+        @Override
+        public void close() throws MaxChangedBlocksException {
+            proxy.close();
+        }
+    }
+
+    public static LevelAndProxy newInstance(EditSession editSession, ServerLevel serverLevel) {
+        FabricServerLevelDelegateProxy proxy = new FabricServerLevelDelegateProxy(editSession, serverLevel);
+        return new LevelAndProxy(
+            (WorldGenLevel) Proxy.newProxyInstance(
+                serverLevel.getClass().getClassLoader(),
+                serverLevel.getClass().getInterfaces(),
+                proxy
+            ),
+            proxy
         );
     }
 
     @Nullable
     private BlockEntity getBlockEntity(BlockPos blockPos) {
-        BlockEntity tileEntity = this.serverLevel.getChunkAt(blockPos).getBlockEntity(blockPos);
-        if (tileEntity == null) {
-            return null;
-        }
-        BlockEntity newEntity = tileEntity.getType().create(blockPos, getBlockState(blockPos));
-        newEntity.loadWithComponents(
-            NBTConverter.toNative(
-                this.editSession.getFullBlock(FabricAdapter.adapt(blockPos)).getNbtReference().getValue()
-            ),
-            this.serverLevel.registryAccess()
-        );
-
-        return newEntity;
+        // This doesn't synthesize or load from world. I think editing existing block entities without setting the block
+        // (in the context of features) should not be supported in the first place.
+        BlockVector3 pos = FabricAdapter.adapt(blockPos);
+        return createdBlockEntities.get(pos);
     }
 
     private BlockState getBlockState(BlockPos blockPos) {
@@ -79,18 +86,38 @@ public class FabricServerLevelDelegateProxy implements InvocationHandler {
 
     private boolean setBlock(BlockPos blockPos, BlockState blockState) {
         try {
+            handleBlockEntity(blockPos, blockState);
             return editSession.setBlock(FabricAdapter.adapt(blockPos), FabricAdapter.adapt(blockState));
         } catch (MaxChangedBlocksException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private boolean removeBlock(BlockPos blockPos, boolean bl) {
-        try {
-            return editSession.setBlock(FabricAdapter.adapt(blockPos), BlockTypes.AIR.getDefaultState());
-        } catch (MaxChangedBlocksException e) {
-            throw new RuntimeException(e);
+    // For BlockEntity#setBlockState, not sure why it's deprecated
+    @SuppressWarnings("deprecation")
+    private void handleBlockEntity(BlockPos blockPos, BlockState blockState) {
+        BlockVector3 pos = FabricAdapter.adapt(blockPos);
+        if (blockState.hasBlockEntity()) {
+            if (!(blockState.getBlock() instanceof EntityBlock entityBlock)) {
+                // This will probably never happen, as Mojang's own code assumes that
+                // hasBlockEntity implies instanceof EntityBlock, but just to be safe...
+                throw new AssertionError("BlockState has block entity but block is not an EntityBlock: " + blockState);
+            }
+            BlockEntity newEntity = entityBlock.newBlockEntity(blockPos, blockState);
+            if (newEntity != null) {
+                newEntity.setBlockState(blockState);
+                createdBlockEntities.put(pos, newEntity);
+                // Should we load existing NBT here? This is for feature / structure gen so it seems unnecessary.
+                // But it would align with the behavior of the real setBlock method.
+                return;
+            }
         }
+        // Discard any block entity that was previously created if new block is set without block entity
+        createdBlockEntities.remove(pos);
+    }
+
+    private boolean removeBlock(BlockPos blockPos, boolean bl) {
+        return setBlock(blockPos, Blocks.AIR.defaultBlockState());
     }
 
     private boolean addEntity(Entity entity) {
@@ -98,6 +125,18 @@ public class FabricServerLevelDelegateProxy implements InvocationHandler {
         Location location = new Location(FabricAdapter.adapt(serverLevel), pos.x(), pos.y(), pos.z());
         BaseEntity baseEntity = new FabricEntity(entity).getState();
         return editSession.createEntity(location, baseEntity) != null;
+    }
+
+    @Override
+    public void close() throws MaxChangedBlocksException {
+        for (Map.Entry<BlockVector3, BlockEntity> entry : createdBlockEntities.entrySet()) {
+            BlockVector3 blockPos = entry.getKey();
+            BlockEntity blockEntity = entry.getValue();
+            editSession.setBlock(
+                blockPos,
+                FabricAdapter.adapt(blockEntity, serverLevel.registryAccess())
+            );
+        }
     }
 
     @Override
@@ -128,7 +167,8 @@ public class FabricServerLevelDelegateProxy implements InvocationHandler {
                     return addEntity(entity);
                 }
             }
-            default -> { }
+            default -> {
+            }
         }
 
         return method.invoke(this.serverLevel, args);
