7dfe07a42e5df234f7bd28734e8710603caabde5
Maddy Miller <mnmiller1@me.com>
2024-12-14 20:44:41 +1000

    IntelliJ is bad

--- worldedit-core/src/main/java/com/sk89q/worldedit/extension/factory/MaskFactory.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/extension/factory/MaskFactory.java
@@ -20,7 +20,22 @@
 package com.sk89q.worldedit.extension.factory;
 
 import com.sk89q.worldedit.WorldEdit;
-import com.sk89q.worldedit.extension.factory.parser.mask.*;
+import com.sk89q.worldedit.extension.factory.parser.mask.AirMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.BiomeMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.BlockCategoryMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.BlockStateMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.BlocksMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.ClipboardMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.ExistingMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.ExposedMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.ExpressionMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.FullCubeMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.LazyRegionMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.NegateMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.NoiseMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.OffsetMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.RegionMaskParser;
+import com.sk89q.worldedit.extension.factory.parser.mask.SolidMaskParser;
 import com.sk89q.worldedit.extension.input.InputParseException;
 import com.sk89q.worldedit.extension.input.NoMatchException;
 import com.sk89q.worldedit.extension.input.ParserContext;
