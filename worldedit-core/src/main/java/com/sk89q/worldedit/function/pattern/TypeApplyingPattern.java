1df5bb2751707bcd97cc860eaa77b2ff22e05f7d
QuImUfu <vo_gel_frei@yahoo.de>
2025-03-14 23:45:01 +0100

    make ^ support nbt (properly)

--- worldedit-core/src/main/java/com/sk89q/worldedit/function/pattern/TypeApplyingPattern.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/function/pattern/TypeApplyingPattern.java
@@ -62,13 +62,13 @@ public class TypeApplyingPattern extends AbstractExtentPattern {
 
     @Override
     public BaseBlock applyBlock(BlockVector3 position) {
-        BlockState oldBlock = getExtent().getBlock(position);
+        BaseBlock oldBlock = getExtent().getFullBlock(position);
         BlockState newBlock = pattern.applyBlock(position).toImmutableState();
         for (Entry<Property<?>, Object> entry : oldBlock.getStates().entrySet()) {
             @SuppressWarnings("unchecked")
             Property<Object> prop = (Property<Object>) entry.getKey();
             newBlock = newBlock.with(prop, entry.getValue());
         }
-        return newBlock.toBaseBlock();
+        return newBlock.toBaseBlock(oldBlock.getNbtReference());
     }
 }
