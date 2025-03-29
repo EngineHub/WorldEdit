b48f160f57e50162d097ea0fbc31a4e1d6c92aa7
Maddy Miller <mnmiller1@me.com>
2025-03-29 16:26:57 +1000

    Gradle 8.13

--- worldedit-bukkit/adapters/adapter-1.21.3/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1.21.3/build.gradle.kts
@@ -6,5 +6,5 @@ plugins {
 
 dependencies {
     // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.21.3-R0.1-20241025.163321-1")
+    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.21.3-R0.1-20250328.155925-87")
 }
