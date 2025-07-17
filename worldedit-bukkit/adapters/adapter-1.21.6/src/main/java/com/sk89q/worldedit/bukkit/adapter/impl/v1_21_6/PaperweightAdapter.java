9164c4649a340164e35f87ce8b6386bc63f5c603
Maddy Miller <git@madelinemiller.dev>
2025-07-17 21:34:53 +1000

    [Bukkit] Warn on version mismatch (#2805)
    * [Bukkit] Warn on version mismatch
    
    * Fix checkstyle & add link

--- worldedit-bukkit/adapters/adapter-1.21.6/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_6/PaperweightAdapter.java
+++ worldedit-bukkit/adapters/adapter-1.21.6/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_6/PaperweightAdapter.java
@@ -212,6 +212,14 @@ public final class PaperweightAdapter implements BukkitImplAdapter {
 
     private static final RandomSource random = RandomSource.create();
 
+    private static final String WRONG_VERSION =
+        """
+        This version of WorldEdit has not been tested with the current Minecraft version.
+        While it may work, there might be unexpected issues.
+        It is recommended to use a version of WorldEdit that supports your Minecraft version.
+        For more information, see https://worldedit.enginehub.org/en/latest/faq/#bukkit-adapters
+        """.stripIndent();
+
     // ------------------------------------------------------------------------
     // Code that may break between versions of Minecraft
     // ------------------------------------------------------------------------
@@ -222,7 +230,7 @@ public final class PaperweightAdapter implements BukkitImplAdapter {
 
         int dataVersion = SharedConstants.getCurrentVersion().dataVersion().version();
         if (dataVersion != Constants.DATA_VERSION_MC_1_21_6 && dataVersion != Constants.DATA_VERSION_MC_1_21_7) {
-            throw new UnsupportedClassVersionError("Not 1.21.6 or 1.21.7!");
+            logger.warning(WRONG_VERSION);
         }
 
         serverWorldsField = CraftServer.class.getDeclaredField("worlds");
