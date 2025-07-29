47a41a471732d67ae047c0b02801d430202ba46f
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2025-07-29 14:40:15 +0100

    fix: 2D polygon volume algorithm
    Co-authored-by: Octavia Togami <otogami@gradle.com>

--- worldedit-core/src/main/java/com/sk89q/worldedit/regions/Polygonal2DRegion.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/regions/Polygonal2DRegion.java
@@ -19,6 +19,7 @@
 
 package com.sk89q.worldedit.regions;
 
+import com.google.common.math.IntMath;
 import com.sk89q.worldedit.math.BlockVector2;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.iterator.FlatRegion3DIterator;
@@ -26,8 +27,6 @@ import com.sk89q.worldedit.regions.iterator.FlatRegionIterator;
 import com.sk89q.worldedit.util.formatting.text.TranslatableComponent;
 import com.sk89q.worldedit.world.World;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -220,11 +219,37 @@ public class Polygonal2DRegion extends AbstractRegion implements FlatRegion {
 
     @Override
     public long getVolume() {
+        if (points.size() <= 2) {
+            return 0;
+        }
+
+        long concreteArea = concreteArea();
+        int b = boundaryPoints();
+        long i = interiorPoints(concreteArea, b);
+        long latticeArea = i + b;
+
+        return latticeArea * (maxY - minY + 1);
+    }
+
+    private int boundaryPoints() {
+        int j = points.size() - 1;
+        int boundaryPoints = 0;
+        for (int i = 0; i < points.size(); i++) {
+            int x = Math.abs(points.get(i).x() - points.get(j).x());
+            int z = Math.abs(points.get(i).z() - points.get(j).z());
+            boundaryPoints += IntMath.gcd(x, z);
+            j = i;
+        }
+
+        return boundaryPoints;
+    }
+
+    // Trapezoid formula
+    private long concreteArea() {
         long area = 0;
-        int i;
         int j = points.size() - 1;
 
-        for (i = 0; i < points.size(); ++i) {
+        for (int i = 0; i < points.size(); i++) {
             BlockVector2 iPoint = points.get(i);
             BlockVector2 jPoint = points.get(j);
             long x = jPoint.x() + (long) iPoint.x();
@@ -233,11 +258,12 @@ public class Polygonal2DRegion extends AbstractRegion implements FlatRegion {
             j = i;
         }
 
-        return BigDecimal.valueOf(area)
-                .multiply(BigDecimal.valueOf(0.5))
-                .abs()
-                .setScale(0, RoundingMode.FLOOR)
-                .longValue() * (maxY - minY + 1);
+        return Math.abs(area) / 2;
+    }
+
+    // Pick's theorem
+    private long interiorPoints(long concreteArea, long boundaryPoints) {
+        return concreteArea - boundaryPoints / 2 + 1;
     }
 
     @Override
