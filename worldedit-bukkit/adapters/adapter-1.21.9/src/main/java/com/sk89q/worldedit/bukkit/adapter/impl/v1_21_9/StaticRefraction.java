5d4c0aee3994e34a199526e7b1d153ed74ec021e
Maddy Miller <git@madelinemiller.dev>
2025-10-08 18:22:33 +1000

    Mark Bukkit adapter as compatible with 1.21.10, and update dependencies

--- worldedit-bukkit/adapters/adapter-1.21.9/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_9/StaticRefraction.java
+++ worldedit-bukkit/adapters/adapter-1.21.9/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_9/StaticRefraction.java
@@ -35,7 +35,7 @@ public final class StaticRefraction {
     public static final String MAIN_THREAD_PROCESSOR = Refraction.pickName(
         "mainThreadProcessor", "g"
     );
-    public static final String NEXT_TICK_TIME = Refraction.pickName("nextTickTime", "e");
+    public static final String NEXT_TICK_TIME = Refraction.pickName("nextTickTimeNanos", "am");
     public static final String GET_BLOCK_STATE = Refraction.pickName("getBlockState", "a_");
     public static final String IS_STATE_AT_POSITION = Refraction.pickName("isStateAtPosition", "a");
     /**
