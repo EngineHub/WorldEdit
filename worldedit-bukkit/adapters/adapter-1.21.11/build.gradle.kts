6f5b0e57946b63f5bdb447580b2286f825662060
Maddy Miller <git@madelinemiller.dev>
2025-12-06 14:22:36 +1000

    [Paper] Initial 1.21.11-rc2 support

---
+++ worldedit-bukkit/adapters/adapter-1.21.11/build.gradle.kts
@@ -0,0 +1,10 @@
+import io.papermc.paperweight.userdev.PaperweightUserDependenciesExtension
+
+plugins {
+    id("buildlogic.adapter")
+}
+
+dependencies {
+    // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
+    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.21.11-rc2-R0.1-20251205.161319-1")
+}
