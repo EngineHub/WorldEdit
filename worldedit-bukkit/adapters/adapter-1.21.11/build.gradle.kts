fc0c65b6208b5c7f3a229f6c687475c763dc9375
Maddy Miller <git@madelinemiller.dev>
2025-12-10 11:19:53 +1000

    Update for release versions

--- worldedit-bukkit/adapters/adapter-1.21.11/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1.21.11/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 }
 
 dependencies {
-    // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.21.11-rc2-R0.1-20251205.161319-1")
+    // https://artifactory.papermc.io/ui/native/universe/io/papermc/paper/dev-bundle/
+    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.21.11-R0.1-20251209.225848-3")
 }
