fe65383d99b306866d080ea405a4e1c4ebe75413
RootBeer <rootb33rman@gmail.com>
2026-03-01 13:13:33 -0500

    Rebase

--- worldedit-bukkit/src/main/java/com/sk89q/worldedit/bukkit/BukkitEntity.java
+++ worldedit-bukkit/src/main/java/com/sk89q/worldedit/bukkit/BukkitEntity.java
@@ -27,9 +27,7 @@ import com.sk89q.worldedit.entity.Player;
 import com.sk89q.worldedit.entity.metadata.EntityProperties;
 import com.sk89q.worldedit.entity.metadata.EntitySchedulerFacet;
 import com.sk89q.worldedit.extent.Extent;
-import com.sk89q.worldedit.regions.RegionOperationException;
 import com.sk89q.worldedit.util.Location;
-import com.sk89q.worldedit.util.formatting.text.TranslatableComponent;
 import com.sk89q.worldedit.world.NullWorld;
 import io.papermc.lib.PaperLib;
 import org.bukkit.Bukkit;
@@ -156,10 +154,6 @@ class BukkitEntity implements Entity {
      */
     @Override
     public boolean remove() {
-        if (WorldEditPlugin.getInstance().isFolia()) {
-            throw new RuntimeException(new RegionOperationException(TranslatableComponent.of("worldedit.bukkit.unsupported-on-folia")));
-        }
-
         org.bukkit.entity.Entity entity = entityRef.get();
         if (entity == null) {
             return true;
