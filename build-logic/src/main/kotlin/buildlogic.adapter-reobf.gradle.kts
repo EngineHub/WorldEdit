cbc335daba642910de8be69f20b2446d5bb7d923
Octavia Togami <octavia.togami@gmail.com>
2026-04-25 17:38:47 -0700

    Move 21 into only old adapters, set launcher for paperweight

--- build-logic/src/main/kotlin/buildlogic.adapter-reobf.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.adapter-reobf.gradle.kts
@@ -6,8 +6,24 @@ plugins {
 
 paperweight {
     reobfArtifactConfiguration = ReobfArtifactConfiguration.REOBF_PRODUCTION
+    javaLauncher = javaToolchains.launcherFor {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 tasks.named("assemble") {
     dependsOn("reobfJar")
 }
+
+java {
+    // Required when we de-sync release option and declared Java versions.
+    disableAutoTargetJvm()
+}
+
+tasks
+    .withType<JavaCompile>()
+    .matching { it.name == "compileJava" || it.name == "compileTestJava" }
+    .configureEach {
+        // We use Java 21 for most of the pre-existing adapters.
+        options.release.set(21)
+    }
