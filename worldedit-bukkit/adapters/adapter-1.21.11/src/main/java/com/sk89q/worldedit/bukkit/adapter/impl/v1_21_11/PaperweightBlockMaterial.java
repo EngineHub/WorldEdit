REZANANS-yep/WorldEdit-Folia

Add BlockMaterial.isFullFace

worldedit-bukkit/adapters/adapter-1.21.11/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_11/PaperweightBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_11;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -55,6 +56,11 @@ public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> 
     @Override
     protected boolean isShapeFullBlock(VoxelShape shape) {
         return Block.isShapeFullBlock(shape);
+    }
+
+    @Override
+    protected boolean isFaceFull(VoxelShape shape, Direction face) {
+        return Block.isFaceFull(shape, PaperweightAdapter.adapt(face));
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.4/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_4/PaperweightBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_4;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -55,6 +56,11 @@ public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> 
     @Override
     protected boolean isShapeFullBlock(VoxelShape shape) {
         return Block.isShapeFullBlock(shape);
+    }
+
+    @Override
+    protected boolean isFaceFull(VoxelShape shape, Direction face) {
+        return Block.isFaceFull(shape, PaperweightAdapter.adapt(face));
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.5/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_5/PaperweightBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_5;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -55,6 +56,11 @@ public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> 
     @Override
     protected boolean isShapeFullBlock(VoxelShape shape) {
         return Block.isShapeFullBlock(shape);
+    }
+
+    @Override
+    protected boolean isFaceFull(VoxelShape shape, Direction face) {
+        return Block.isFaceFull(shape, PaperweightAdapter.adapt(face));
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.6/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_6/PaperweightBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_6;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -55,6 +56,11 @@ public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> 
     @Override
     protected boolean isShapeFullBlock(VoxelShape shape) {
         return Block.isShapeFullBlock(shape);
+    }
+
+    @Override
+    protected boolean isFaceFull(VoxelShape shape, Direction face) {
+        return Block.isFaceFull(shape, PaperweightAdapter.adapt(face));
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.9/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_9/PaperweightBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_9;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -55,6 +56,11 @@ public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> 
     @Override
     protected boolean isShapeFullBlock(VoxelShape shape) {
         return Block.isShapeFullBlock(shape);
+    }
+
+    @Override
+    protected boolean isFaceFull(VoxelShape shape, Direction face) {
+        return Block.isFaceFull(shape, PaperweightAdapter.adapt(face));
     }
 
     @Override

worldedit-core-mc/src/main/java/com/sk89q/worldedit/coremc/internal/CoreMcBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.coremc.internal;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -59,6 +60,11 @@ public final class CoreMcBlockMaterial extends AbstractBlockMaterial<VoxelShape>
     @Override
     protected boolean isShapeFullBlock(VoxelShape shape) {
         return Block.isShapeFullBlock(shape);
+    }
+
+    @Override
+    protected boolean isFaceFull(VoxelShape shape, Direction face) {
+        return Block.isFaceFull(shape, CoreMcTransmogrifier.transmogToMinecraft(face));
     }
 
     @Override

EngineHub/WorldEdit

worldedit-bukkit/adapters/adapter-1.21.11/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_11/PaperweightBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_11;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -54,6 +55,11 @@ public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> 
     @Override
     protected boolean isFullCubeUncached(ShapeType shapeType) {
         return Block.isShapeFullBlock(getShape(shapeType));
+    }
+
+    @Override
+    protected boolean isFullFaceUncached(ShapeType shapeType, Direction face) {
+        return Block.isFaceFull(getShape(shapeType), PaperweightAdapter.adapt(face));
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.4/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_4/PaperweightBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_4;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -54,6 +55,11 @@ public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> 
     @Override
     protected boolean isFullCubeUncached(ShapeType shapeType) {
         return Block.isShapeFullBlock(getShape(shapeType));
+    }
+
+    @Override
+    protected boolean isFullFaceUncached(ShapeType shapeType, Direction face) {
+        return Block.isFaceFull(getShape(shapeType), PaperweightAdapter.adapt(face));
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.5/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_5/PaperweightBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_5;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -54,6 +55,11 @@ public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> 
     @Override
     protected boolean isFullCubeUncached(ShapeType shapeType) {
         return Block.isShapeFullBlock(getShape(shapeType));
+    }
+
+    @Override
+    protected boolean isFullFaceUncached(ShapeType shapeType, Direction face) {
+        return Block.isFaceFull(getShape(shapeType), PaperweightAdapter.adapt(face));
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.6/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_6/PaperweightBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_6;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -54,6 +55,11 @@ public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> 
     @Override
     protected boolean isFullCubeUncached(ShapeType shapeType) {
         return Block.isShapeFullBlock(getShape(shapeType));
+    }
+
+    @Override
+    protected boolean isFullFaceUncached(ShapeType shapeType, Direction face) {
+        return Block.isFaceFull(getShape(shapeType), PaperweightAdapter.adapt(face));
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.9/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_9/PaperweightBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_9;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -54,6 +55,11 @@ public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> 
     @Override
     protected boolean isFullCubeUncached(ShapeType shapeType) {
         return Block.isShapeFullBlock(getShape(shapeType));
+    }
+
+    @Override
+    protected boolean isFullFaceUncached(ShapeType shapeType, Direction face) {
+        return Block.isFaceFull(getShape(shapeType), PaperweightAdapter.adapt(face));
     }
 
     @Override

worldedit-core-mc/src/main/java/com/sk89q/worldedit/coremc/internal/CoreMcBlockMaterial.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.coremc.internal;
 
 import com.sk89q.worldedit.blocks.ShapeType;
 import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
+import com.sk89q.worldedit.util.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -58,6 +59,11 @@ public final class CoreMcBlockMaterial extends AbstractBlockMaterial<VoxelShape>
     @Override
     protected boolean isFullCubeUncached(ShapeType shapeType) {
         return Block.isShapeFullBlock(getShape(shapeType));
+    }
+
+    @Override
+    protected boolean isFullFaceUncached(ShapeType shapeType, Direction face) {
+        return Block.isFaceFull(getShape(shapeType), CoreMcTransmogrifier.transmogToMinecraft(face));
     }
 
     @Override