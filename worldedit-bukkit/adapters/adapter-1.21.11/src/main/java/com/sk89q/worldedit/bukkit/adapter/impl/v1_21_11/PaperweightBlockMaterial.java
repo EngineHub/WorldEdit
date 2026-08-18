REZANANS-yep/WorldEdit-Folia

Add ShapeType parameter to BlockMaterial.isFullCube

worldedit-bukkit/adapters/adapter-1.21.11/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_11/PaperweightBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_11;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,8 +28,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class PaperweightBlockMaterial implements BlockMaterial {
+public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -42,8 +45,16 @@ public class PaperweightBlockMaterial implements BlockMaterial {
     }
 
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
+    @Override
+    protected boolean isShapeFullBlock(VoxelShape shape) {
+        return Block.isShapeFullBlock(shape);
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.5/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_5/PaperweightBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_5;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,8 +28,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class PaperweightBlockMaterial implements BlockMaterial {
+public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -42,8 +45,16 @@ public class PaperweightBlockMaterial implements BlockMaterial {
     }
 
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
+    @Override
+    protected boolean isShapeFullBlock(VoxelShape shape) {
+        return Block.isShapeFullBlock(shape);
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.6/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_6/PaperweightBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_6;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,8 +28,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class PaperweightBlockMaterial implements BlockMaterial {
+public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -42,8 +45,16 @@ public class PaperweightBlockMaterial implements BlockMaterial {
     }
 
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
+    @Override
+    protected boolean isShapeFullBlock(VoxelShape shape) {
+        return Block.isShapeFullBlock(shape);
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.9/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_9/PaperweightBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_9;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,8 +28,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class PaperweightBlockMaterial implements BlockMaterial {
+public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -42,8 +45,16 @@ public class PaperweightBlockMaterial implements BlockMaterial {
     }
 
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
+    @Override
+    protected boolean isShapeFullBlock(VoxelShape shape) {
+        return Block.isShapeFullBlock(shape);
     }
 
     @Override

worldedit-core-mc/src/main/java/com/sk89q/worldedit/coremc/internal/CoreMcBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.coremc.internal;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,12 +28,14 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 /**
  * Minecraft block material implementation for platforms sharing native code.
  * Pulls as much info as possible from the Minecraft BlockState.
  */
-public final class CoreMcBlockMaterial implements BlockMaterial {
+public final class CoreMcBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -46,8 +49,16 @@ public final class CoreMcBlockMaterial implements BlockMaterial {
     }
 
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
+    @Override
+    protected boolean isShapeFullBlock(VoxelShape shape) {
+        return Block.isShapeFullBlock(shape);
     }
 
     @Override

worldedit-core/src/main/java/com/sk89q/worldedit/internal/block/AbstractBlockMaterial.java
@@ -1,0 +1,49 @@
+/*
+ * WorldEdit, a Minecraft world manipulation toolkit
+ * Copyright (C) sk89q <http://www.sk89q.com>
+ * Copyright (C) WorldEdit team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.sk89q.worldedit.internal.block;
+
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.util.concurrency.LazyReference;
+import com.sk89q.worldedit.world.registry.BlockMaterial;
+
+import java.util.EnumSet;
+
+public abstract class AbstractBlockMaterial<VS> implements BlockMaterial {
+    @SuppressWarnings("this-escape")
+    public LazyReference<EnumSet<ShapeType>> isFullCube = LazyReference.from(() -> {
+        EnumSet<ShapeType> enumSet = EnumSet.noneOf(ShapeType.class);
+        for (ShapeType shapeType : ShapeType.values()) {
+            if (isShapeFullBlock(getShape(shapeType))) {
+                enumSet.add(shapeType);
+            }
+        }
+
+        return enumSet;
+    });
+
+    @Override
+    public boolean isFullCube(ShapeType shapeType) {
+        return isFullCube.getValue().contains(shapeType);
+    }
+
+    protected abstract VS getShape(ShapeType shapeType);
+
+    protected abstract boolean isShapeFullBlock(VS shape);
+}

worldedit-sponge/src/main/java/com/sk89q/worldedit/sponge/SpongeBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.sponge;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,13 +28,15 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 /**
  * Sponge block material that pulls as much info as possible from the Minecraft
  * Material, and passes the rest to another implementation, typically the
  * bundled block info.
  */
-public class SpongeBlockMaterial implements BlockMaterial {
+public class SpongeBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -47,8 +50,16 @@ public class SpongeBlockMaterial implements BlockMaterial {
     }
 
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
+    @Override
+    protected boolean isShapeFullBlock(VoxelShape shape) {
+        return Block.isShapeFullBlock(shape);
     }
 
     @Override

EngineHub/WorldEdit

worldedit-bukkit/adapters/adapter-1.21.11/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_11/PaperweightBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_11;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,8 +28,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class PaperweightBlockMaterial implements BlockMaterial {
+public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -41,9 +44,16 @@ public class PaperweightBlockMaterial implements BlockMaterial {
         return block.isAir();
     }
 
+    private VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected boolean isFullCubeUncached(ShapeType shapeType) {
+        return Block.isShapeFullBlock(getShape(shapeType));
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.5/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_5/PaperweightBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_5;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,8 +28,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class PaperweightBlockMaterial implements BlockMaterial {
+public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -41,9 +44,16 @@ public class PaperweightBlockMaterial implements BlockMaterial {
         return block.isAir();
     }
 
+    private VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected boolean isFullCubeUncached(ShapeType shapeType) {
+        return Block.isShapeFullBlock(getShape(shapeType));
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.6/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_6/PaperweightBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_6;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,8 +28,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class PaperweightBlockMaterial implements BlockMaterial {
+public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -41,9 +44,16 @@ public class PaperweightBlockMaterial implements BlockMaterial {
         return block.isAir();
     }
 
+    private VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected boolean isFullCubeUncached(ShapeType shapeType) {
+        return Block.isShapeFullBlock(getShape(shapeType));
     }
 
     @Override

worldedit-bukkit/adapters/adapter-1.21.9/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/v1_21_9/PaperweightBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.bukkit.adapter.impl.v1_21_9;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,8 +28,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class PaperweightBlockMaterial implements BlockMaterial {
+public class PaperweightBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -41,9 +44,16 @@ public class PaperweightBlockMaterial implements BlockMaterial {
         return block.isAir();
     }
 
+    private VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected boolean isFullCubeUncached(ShapeType shapeType) {
+        return Block.isShapeFullBlock(getShape(shapeType));
     }
 
     @Override

worldedit-core-mc/src/main/java/com/sk89q/worldedit/coremc/internal/CoreMcBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.coremc.internal;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,12 +28,14 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 /**
  * Minecraft block material implementation for platforms sharing native code.
  * Pulls as much info as possible from the Minecraft BlockState.
  */
-public final class CoreMcBlockMaterial implements BlockMaterial {
+public final class CoreMcBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -45,9 +48,16 @@ public final class CoreMcBlockMaterial implements BlockMaterial {
         return block.isAir();
     }
 
+    private VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected boolean isFullCubeUncached(ShapeType shapeType) {
+        return Block.isShapeFullBlock(getShape(shapeType));
     }
 
     @Override

worldedit-core/src/main/java/com/sk89q/worldedit/internal/block/AbstractBlockMaterial.java
@@ -17,24 +17,31 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.sk89q.worldedit.function.mask;
+package com.sk89q.worldedit.internal.block;
 
 import com.sk89q.worldedit.blocks.ShapeType;
-import com.sk89q.worldedit.extent.Extent;
-import com.sk89q.worldedit.math.BlockVector3;
-import com.sk89q.worldedit.world.block.BlockState;
+import com.sk89q.worldedit.util.concurrency.LazyReference;
+import com.sk89q.worldedit.world.registry.BlockMaterial;
 
-public final class FullCubeMask extends AbstractExtentMask {
+import java.util.EnumSet;
 
-    public FullCubeMask(Extent extent) {
-        super(extent);
-    }
+public abstract class AbstractBlockMaterial<VS> implements BlockMaterial {
+    @SuppressWarnings("this-escape")
+    public LazyReference<EnumSet<ShapeType>> isFullCube = LazyReference.from(() -> {
+        EnumSet<ShapeType> enumSet = EnumSet.noneOf(ShapeType.class);
+        for (ShapeType shapeType : ShapeType.values()) {
+            if (isFullCubeUncached(shapeType)) {
+                enumSet.add(shapeType);
+            }
+        }
+
+        return enumSet;
+    });
 
     @Override
-    public boolean test(BlockVector3 vector) {
-        Extent extent = getExtent();
-        BlockState block = extent.getBlock(vector);
-        return block.getBlockType().getMaterial().isFullCube(ShapeType.SHAPE);
+    public boolean isFullCube(ShapeType shapeType) {
+        return isFullCube.getValue().contains(shapeType);
     }
 
+    protected abstract boolean isFullCubeUncached(ShapeType shapeType);
 }

worldedit-sponge/src/main/java/com/sk89q/worldedit/sponge/SpongeBlockMaterial.java
@@ -19,7 +19,8 @@
 
 package com.sk89q.worldedit.sponge;
 
-import com.sk89q.worldedit.world.registry.BlockMaterial;
+import com.sk89q.worldedit.blocks.ShapeType;
+import com.sk89q.worldedit.internal.block.AbstractBlockMaterial;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.level.EmptyBlockGetter;
@@ -27,13 +28,15 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 /**
  * Sponge block material that pulls as much info as possible from the Minecraft
  * Material, and passes the rest to another implementation, typically the
  * bundled block info.
  */
-public class SpongeBlockMaterial implements BlockMaterial {
+public class SpongeBlockMaterial extends AbstractBlockMaterial<VoxelShape> {
 
     private final BlockState block;
 
@@ -46,9 +49,16 @@ public class SpongeBlockMaterial implements BlockMaterial {
         return block.isAir();
     }
 
+    private VoxelShape getShape(ShapeType shapeType) {
+        return switch (shapeType) {
+            case SHAPE -> block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+            case VISUAL_SHAPE -> block.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty());
+        };
+    }
+
     @Override
-    public boolean isFullCube() {
-        return Block.isShapeFullBlock(block.getShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO));
+    protected boolean isFullCubeUncached(ShapeType shapeType) {
+        return Block.isShapeFullBlock(getShape(shapeType));
     }
 
     @Override