28a9bef90045138e75099363cbb25427da252a3b
Maddy Miller <git@madelinemiller.dev>
2026-09-19 21:22:14 +1000

    [Sponge] 26.3 support

--- worldedit-sponge/src/main/java/com/sk89q/worldedit/sponge/SpongeBlockMaterial.java
+++ worldedit-sponge/src/main/java/com/sk89q/worldedit/sponge/SpongeBlockMaterial.java
@@ -95,12 +95,12 @@ public class SpongeBlockMaterial implements BlockMaterial {
 
     @Override
     public boolean isFragileWhenPushed() {
-        return block.getPistonPushReaction() == PushReaction.DESTROY;
+        return block.getPistonPushReaction() == PushReaction.POPPED;
     }
 
     @Override
     public boolean isUnpushable() {
-        return block.getPistonPushReaction() == PushReaction.BLOCK;
+        return block.getPistonPushReaction() == PushReaction.IMMOVEABLE;
     }
 
     @Override
@@ -111,7 +111,7 @@ public class SpongeBlockMaterial implements BlockMaterial {
     @Override
     @SuppressWarnings("deprecation")
     public boolean isMovementBlocker() {
-        return block.blocksMotion();
+        return block.isSolid();
     }
 
     @Override
